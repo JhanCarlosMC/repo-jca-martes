bedb66d4010fe518ad99c70cf18438a745b382f7
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2026-08-18 18:03:46 -0500

    Feat: introduccion a JS

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//Variables En js
+//1.Palabra reservada "let"
+//2.Nombre de la variable
+//3.Simbolo "="
+//4.El valor que se va a guardar
+//5.Punto y Coma 
+//      para finalizar la linea de codigo
+//Inicializar
+let nombre = "Jhan Carlos"; //
+let apellido = "Martinez Ceballos";
+const CEDULA = 1001010101;
+
+const CANT_HABITACIONES = 4;
+let cantHabitacionesDispo = 1;
+const OPCIONES = ["opcion1", "Opcion2"];
+const OP1 = "Opcion1";
+const OP2 = "Opcion2";
+
+//Declarar
+let ciudadNacimiento;
+const FECHA_NACIMIENTO = "hoy";
+
+//Asignar
+ciudadNacimiento = "Armenia";
+
+//Debbug
+//console.log("Hola");
+let totalHabitacionesOcupadas = CANT_HABITACIONES - cantHabitacionesDispo;
+//console.log("Habitaciones Disponibles: " + cantHabitacionesDispo)
+//console.log("Habitaciones Totales: " + CANT_HABITACIONES);
+//console.log("Habitaciones Ocupadas: " + totalHabitacionesOcupadas);
+
+//Operadores
+//Aritmeticos -> + - * / % = **(Math.pow())
+//Logicos -> ||(Or) &&(And) !(Negacion)
+//Relacionales -> < > <= >= == != ===
+console.log(5 == "5");
+console.log(5 === "5");
+
